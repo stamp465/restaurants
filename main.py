3a383fce3849a39dce5a12e1660c2c9d9b97f889
Shasn <40135787+stamp465@users.noreply.github.com>
2022-02-06 15:47:53 +0700

    first commit

--- main.py
+++ main.py
@@ -1,41 +1,102 @@
-from fastapi import FastAPI
+from shelve import BsdDbShelf
 from pymongo import MongoClient
+
+from fastapi import FastAPI, HTTPException
+from typing import Optional
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
 
 class Reservation(BaseModel):
     name : str
     time: int
     table_number: int
     
-client = MongoClient('mongodb://localhost', 27017)
+client = MongoClient('mongodb://localhost', 27018)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["reservation"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reserve"]
 
 app = FastAPI()
 
+@app.get("/")
+def get_roots():
+    return "Welcome"
+
+@app.get("/reservation")
+def get_all_data():
+    result = collection.find({},{"_id":0})
+    dic = []
+    for r in result:
+        print(r)
+        dic.append(r)
+    return dic
 
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    result = collection.find({"name":name},{"_id":0})
+    dic = []
+    for r in result:
+        print(r)
+        dic.append(r)
+    return dic
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    result = collection.find({"table_number":table},{"_id":0})
+    dic = []
+    for r in result:
+        print(r)
+        dic.append(r)
+    return dic
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
-    pass
+    result = collection.find({"table_number":reservation.table_number},{"_id":0})
+    for i in result :
+        if reservation.time == i["time"] :
+            return {
+                f'''Suppose that on table number {reservation.table_number} has a reservation at {reservation.time}:00. You can't make a reservation on that time and that table.'''
+            }
+    a = jsonable_encoder(reservation)
+    print(a)
+    collection.insert_one(a)
+    return {
+        "result" : "reserve done"
+    }
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    pass
+    myquery = { 
+        "name": reservation.name,
+        "table_number" : reservation.table_number
+    }
+    newvalues = { "$set": { "time": reservation.time } }
+    
+    result = collection.find({"table_number":reservation.table_number},{"_id":0})
+    for i in result :
+        if reservation.time == i["time"] :
+            return {
+                f'''Suppose that on table number {reservation.table_number} has a reservation at {reservation.time}:00. You can't make a reservation on that time and that table.'''
+            }
+    print("can up ?")
+    print(myquery,newvalues)
+    collection.update_one(myquery, newvalues)
+    return {
+        "result" : "update_one done"
+    }
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
+    data = {
+        "name" : name,
+        "table_number" : table_number
+    }
+    collection.delete_one(data)
+    return {
+        "result" : "delete done"
+    }
 
